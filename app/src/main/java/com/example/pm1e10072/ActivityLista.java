77a1597ba7a2ba9037f30569cc04b9012ecaf1d5
sussanhernandez <hernandezsussan792@gmail.com>
2023-02-18 00:51:35 -0600

    Funcion eliminar contacto terminada

--- app/src/main/java/com/example/pm1e10072/ActivityLista.java
+++ app/src/main/java/com/example/pm1e10072/ActivityLista.java
@@ -5,8 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.pm1e10072.tablas.Contactos;
 import com.example.pm1e10072.transacciones.Transacciones;
@@ -19,6 +23,8 @@ public class ActivityLista extends AppCompatActivity {
     ListView listViewListaContactos;
     ArrayList<Contactos> arrayListContactos;
     ArrayList<String> arrayListContactosToString;
+    int posicionDeFilaEnLista;
+    ArrayAdapter<String> arrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,8 +36,31 @@ public class ActivityLista extends AppCompatActivity {
 
         obtenerListaContactos();
 
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, arrayListContactosToString);
+        arrayAdapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, arrayListContactosToString);
         listViewListaContactos.setAdapter(arrayAdapter);
+
+        listViewListaContactos.setOnItemClickListener((adapterView, view, i, l) -> {
+            posicionDeFilaEnLista = arrayListContactos.get(i).getId();
+            String positionString = String.valueOf(posicionDeFilaEnLista);
+            Toast.makeText(getApplicationContext(), positionString, Toast.LENGTH_SHORT).show();
+            arrayAdapter.notifyDataSetChanged();
+        });
+
+        Button btnBorrar = findViewById(R.id.btnBorrar);
+        btnBorrar.setOnClickListener(view -> {
+            BorrarContacto();
+            arrayAdapter.notifyDataSetChanged();
+            listViewListaContactos.setAdapter(arrayAdapter);
+        });
+    }
+
+    private void BorrarContacto() {
+        SQLiteDatabase db = conexion.getWritableDatabase();
+        String[] parameterId = {String.valueOf(posicionDeFilaEnLista)};
+
+        db.delete(Transacciones.tablaContactos, Transacciones.id + "=?", parameterId);
+        Toast.makeText(getApplicationContext(), "Contacto Eliminado", Toast.LENGTH_SHORT).show();
+        arrayAdapter.notifyDataSetChanged();
     }
 
     private void obtenerListaContactos() {
